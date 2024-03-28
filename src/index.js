cb4b69f913d3c3af731cdf3e968b25703f0faea1
Binayak <kunonner2002@gmail.com>
2024-03-28 08:54:12 +0545

    Setup and Configuration Completed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path:'./env'
 })
 
-connectDB()
+// connectDB().then(() => {
+//     try{
+//         app.on("error",(error) => {
+//             console.log("ERR:",error);
+//             throw error
+//         })
+//         app.listen(process.env.PORT || 8000, () => {
+//             console.log(`Server is running at port : ${process.env.PORT}`);
+//         } )
+//     }
+//     catch((err) => {
+//         console.log("MONGO db connection failed !!! ",err);
+//     })
+// })
+connectDB().then(() => {
+    try {
+        app.on("error", (error) => {
+            console.log("ERR:", error);
+            throw error;
+        });
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port : ${process.env.PORT || 8000}`);
+        });
+    } catch (err) {
+        console.log("MONGO db connection failed !!! ", err);
+    }
+});
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs"
+
+cloudinary.config({ 
+  cloud_name: 'process.env.CLOUDINARY_CLOUD_NAME', 
+  api_key: 'process.env.CLOUDINARY_API_KEY', 
+  api_secret: 'process.env.CLOUDINARY_API_SECRET' 
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try{
+        if(!localFilePath) return null
+        // upload the file on cloudinary
+        const uploadedResponse = await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        }) 
+        // if file uploaded successfully via localFilePath
+        console.log("File Has Been Uploaded Successfully", uploadedResponse.url);
+        return uploadedResponse;
+    }catch(error){
+        // removing the temp file from local server after the file have been uploaded to cloudinary
+        fs.unlinkSync(localFilePath)
+        //remove the locally saved temporary file as the upload opeartion got failed
+        return null;
+
+    }
+}
+
+export { uploadOnCloudinary }
